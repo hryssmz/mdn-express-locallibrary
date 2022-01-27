287f035a97d92ed3cdeff29e19a9745c4f4b1a8e
Hiroyasu Shimizu <hryssmz@yahoo.com>
2022-01-28 02:25:57 +0900

    refactor: Refactor BookInstance views

--- src/controllers/bookInstanceController.ts
+++ src/controllers/bookInstanceController.ts
@@ -1,8 +1,9 @@
 // controllers/bookInstanceController.ts
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
-import { bookInstanceValidator } from "../validators/bookInstanceValidator";
 import createError from "http-errors";
+import { Types } from "mongoose";
+import { bookInstanceValidator } from "../validators/bookInstanceValidator";
 import Book from "../models/book";
 import BookInstance, { statusChoices } from "../models/bookInstance";
 
@@ -10,6 +11,7 @@ export const bookInstanceList = async (req: Request, res: Response) => {
   const bookInstanceList = await BookInstance.find().populate<{ book: Book }>(
     "book"
   );
+  // HTTP 200: render book copy list
   return res.render("bookInstanceList", {
     title: "Book Instance List",
     bookInstanceList,
@@ -22,23 +24,28 @@ export const bookInstanceDetail = async (
   next: NextFunction
 ) => {
   try {
-    const bookInstance = await BookInstance.findById(req.params.id).populate<{
-      book: Book;
-    }>("book");
-    if (bookInstance === null) {
-      return next(createError(404, "Book copy not found"));
-    }
-    return res.render("bookInstanceDetail", {
-      title: `Copy: ${bookInstance.book.title}`,
-      bookInstance,
-    });
+    new Types.ObjectId(req.params.id);
   } catch (err) {
-    return next(err);
+    // HTTP 404: bad ID provided
+    return next(createError(404, "Book copy not found"));
   }
+  const bookInstance = await BookInstance.findById(req.params.id).populate<{
+    book: Book;
+  }>("book");
+  if (bookInstance === null) {
+    // HTTP 404: book copy not found
+    return next(createError(404, "Book copy not found"));
+  }
+  // HTTP 200: render book copy
+  return res.render("bookInstanceDetail", {
+    title: `Copy: ${bookInstance.book.title}`,
+    bookInstance,
+  });
 };
 
 export const bookInstanceCreateGet = async (req: Request, res: Response) => {
   const bookList = await Book.find({}, "title").sort({ title: 1 });
+  // HTTP 200: return books and status choices
   return res.render("bookInstanceForm", {
     title: "Create BookInstance",
     bookList,
@@ -46,29 +53,29 @@ export const bookInstanceCreateGet = async (req: Request, res: Response) => {
   });
 };
 
-export const bookInstanceCreate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const bookInstanceCreate = async (req: Request, res: Response) => {
   await bookInstanceValidator.run(req);
   const errors = validationResult(req);
+  const bookInstanceData = {
+    book: req.body.book,
+    imprint: req.body.imprint,
+    status: req.body.status,
+    dueBack: req.body.dueBack,
+  };
   if (!errors.isEmpty()) {
     const bookList = await Book.find({}, "title").sort({ title: 1 });
+    // HTTP 200: render choices, book copy, and validation errors
     return res.render("bookInstanceForm", {
       title: "Create BookInstance",
       bookList,
       statusChoices,
-      bookInstance: req.body,
+      bookInstance: bookInstanceData,
       errors: errors.mapped(),
     });
   }
-  try {
-    const bookInstance = await BookInstance.create(req.body);
-    return res.redirect(bookInstance.url);
-  } catch (err) {
-    return next(err);
-  }
+  // HTTP 302: create book copy and redirect to detail view
+  const bookInstance = await BookInstance.create(req.body);
+  return res.redirect(bookInstance.url);
 };
 
 export const bookInstanceUpdateGet = async (
@@ -77,22 +84,26 @@ export const bookInstanceUpdateGet = async (
   next: NextFunction
 ) => {
   try {
-    const [bookInstance, bookList] = await Promise.all([
-      BookInstance.findById(req.params.id),
-      Book.find({}, "title").sort({ title: 1 }),
-    ]);
-    if (bookInstance === null) {
-      return next(createError(404, "BookInstance not found"));
-    }
-    return res.render("bookInstanceForm", {
-      title: "Update BookInstance",
-      bookInstance,
-      bookList,
-      statusChoices,
-    });
+    new Types.ObjectId(req.params.id);
   } catch (err) {
-    return next(err);
+    // HTTP 404: bad ID provided
+    return res.status(404).json("Book copy not found");
   }
+  const [bookInstance, bookList] = await Promise.all([
+    BookInstance.findById(req.params.id),
+    Book.find({}, "title").sort({ title: 1 }),
+  ]);
+  if (bookInstance === null) {
+    // HTTP 404: book copy not found
+    return next(createError(404, "Book copy not found"));
+  }
+  // HTTP 200: render books, status choices and book copy
+  return res.render("bookInstanceForm", {
+    title: "Update BookInstance",
+    bookInstance,
+    bookList,
+    statusChoices,
+  });
 };
 
 export const bookInstanceUpdate = async (
@@ -100,10 +111,28 @@ export const bookInstanceUpdate = async (
   res: Response,
   next: NextFunction
 ) => {
+  try {
+    new Types.ObjectId(req.params.id);
+  } catch (err) {
+    // HTTP 404: bad ID provided
+    return next(createError(404, "Book copy not found"));
+  }
+  const bookInstance = await BookInstance.findById(req.params.id);
+  if (bookInstance === null) {
+    // HTTP 404: book copy not found
+    return next(createError(404, "Book copy not found"));
+  }
   await bookInstanceValidator.run(req);
   const errors = validationResult(req);
+  const bookInstanceData = {
+    book: req.body.book,
+    imprint: req.body.imprint,
+    status: req.body.status,
+    dueBack: req.body.dueBack,
+  };
   if (!errors.isEmpty()) {
     const bookList = await Book.find({}, "title").sort({ title: 1 });
+    // HTTP 200: render choices, book copy, and validation errors
     return res.render("bookInstanceForm", {
       title: "Update BookInstance",
       bookList,
@@ -112,48 +141,43 @@ export const bookInstanceUpdate = async (
       errors: errors.mapped(),
     });
   }
-  try {
-    const bookInstance = await BookInstance.findByIdAndUpdate(
-      req.params.id,
-      req.body
-    );
-    if (bookInstance === null) {
-      return next(createError(404, "BookInstance not found"));
-    }
-    return res.redirect(bookInstance.url);
-  } catch (err) {
-    return next(err);
-  }
+  // HTTP 302: update book copy and redirect to detail view
+  await bookInstance.updateOne(bookInstanceData);
+  return res.redirect(bookInstance.url);
 };
 
-export const bookInstanceDeleteGet = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const bookInstanceDeleteGet = async (req: Request, res: Response) => {
   try {
-    const bookInstance = await BookInstance.findById(req.params.id);
-    if (bookInstance === null) {
-      return res.redirect("/catalog/book-instances");
-    }
-    return res.render("bookInstanceDelete", {
-      title: "Delete BookInstance",
-      bookInstance,
-    });
+    new Types.ObjectId(req.params.id);
   } catch (err) {
-    return next(err);
-  }
-};
-
-export const bookInstanceDelete = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    await BookInstance.findByIdAndRemove(req.body.bookInstanceId);
+    // HTTP 302: redirect to list view if bad ID provided
     return res.redirect("/catalog/book-instances");
-  } catch (err) {
-    return next(err);
   }
+  const bookInstance = await BookInstance.findById(req.params.id);
+  if (bookInstance === null) {
+    // HTTP 302: redirect to list view if book copy not found
+    return res.redirect("/catalog/book-instances");
+  }
+  // HTTP 200: render book copy
+  return res.render("bookInstanceDelete", {
+    title: "Delete BookInstance",
+    bookInstance,
+  });
+};
+
+export const bookInstanceDelete = async (req: Request, res: Response) => {
+  try {
+    new Types.ObjectId(req.body.bookInstanceId);
+  } catch (err) {
+    // HTTP 302: redirect to list view if bad ID provided
+    return res.redirect("/catalog/book-instances");
+  }
+  const bookInstance = BookInstance.findById(req.body.bookInstanceId);
+  if (bookInstance === null) {
+    // HTTP 302: redirect to list view if book copy not found
+    return res.redirect("/catalog/book-instances");
+  }
+  // HTTP 302: delete book and redirect to list view
+  await bookInstance.deleteOne();
+  return res.redirect("/catalog/book-instances");
 };
--- src/validators/bookInstanceValidator.spec.ts
+++ src/validators/bookInstanceValidator.spec.ts
@@ -56,4 +56,88 @@ describe("valid requests", () => {
       status: "Maintenance",
     });
   });
+
+  test("empty string date input will be ignored", async () => {
+    const book = new Types.ObjectId();
+    const res = await request(app).post("/").send({
+      book: book._id,
+      imprint: "Foo Imprint",
+      dueBack: "",
+    });
+
+    expect(res.body.errors).toStrictEqual({});
+    expect(res.body.body).toStrictEqual({
+      book: String(book._id),
+      imprint: "Foo Imprint",
+      status: "Maintenance",
+    });
+  });
+
+  test("null date input will be ignored", async () => {
+    const book = new Types.ObjectId();
+    const res = await request(app).post("/").send({
+      book: book._id,
+      imprint: "Foo Imprint",
+      dueBack: null,
+    });
+
+    expect(res.body.errors).toStrictEqual({});
+    expect(res.body.body).toStrictEqual({
+      book: String(book._id),
+      imprint: "Foo Imprint",
+      status: "Maintenance",
+    });
+  });
+});
+
+describe("invalid requests", () => {
+  test("book and imprint not specified", async () => {
+    const res = await request(app).post("/");
+
+    expect(res.body.errors).toStrictEqual({
+      book: {
+        msg: "Book must be specified",
+        value: "",
+      },
+      imprint: {
+        msg: "Imprint must be specified",
+        value: "",
+      },
+    });
+    expect(res.body.body).toStrictEqual({
+      book: "",
+      imprint: "",
+      status: "Maintenance",
+    });
+  });
+
+  test("invalid book ID, status, date specified", async () => {
+    const res = await request(app).post("/").send({
+      book: "badObjectId",
+      imprint: "Foo Imprint",
+      status: "BadStatus",
+      dueBack: "INVALID",
+    });
+
+    expect(res.body.errors).toStrictEqual({
+      book: {
+        msg: "Please specify a valid Mongo ID",
+        value: "badObjectId",
+      },
+      status: {
+        msg: "Status 'BadStatus' is not allowed",
+        value: "BadStatus",
+      },
+      dueBack: {
+        msg: "Invalid date",
+        value: "INVALID",
+      },
+    });
+    expect(res.body.body).toStrictEqual({
+      book: "badObjectId",
+      imprint: "Foo Imprint",
+      status: "BadStatus",
+      dueBack: "INVALID",
+    });
+  });
 });
